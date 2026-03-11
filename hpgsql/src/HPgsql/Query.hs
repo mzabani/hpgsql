8707536ebea90a3779bb5f961e9de0724cd6d582
Marcelo Zabani <mzabani@gmail.com>
2026-03-11 17:04:22 -0300

    Improve Query parsing code

--- hpgsql/src/HPgsql/Query.hs
+++ hpgsql/src/HPgsql/Query.hs
@@ -9,11 +9,15 @@ module HPgsql.Query
   )
 where
 
+import Control.Applicative (some, (<|>))
+import Data.Attoparsec.Text (Parser)
+import qualified Data.Attoparsec.Text as A
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.List as List
 import Data.List.NonEmpty (NonEmpty (..))
 import qualified Data.List.NonEmpty as NE
+import Data.Proxy (Proxy (..))
 import Data.String (IsString (..))
 import Data.Text (Text)
 import qualified Data.Text as Text
@@ -24,9 +28,10 @@ import HPgsql.TypeInfo (Oid)
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
 
--- | Represents fragments of SQL text that can include interpolated variables
-data SqlFragment = LiteralText !Text | InterpolatedVar !String
-  deriving stock (Show, Eq)
+-- | Represents fragments of SQL text that can include interpolated Haskell expressions
+-- or fragments of plain SQL.
+data SqlFragment = NonInterpolatedSqlFragment !Text | InterpolatedHaskellExpr !Text
+  deriving stock (Eq)
 
 newtype Query = Query (NonEmpty SingleQuery)
   deriving newtype (Eq, Semigroup, Show)
@@ -94,26 +99,26 @@ liftQueries :: NonEmpty SqlStatement -> Q Exp
 liftQueries statements = do
   queryExps <- mapM liftQuery statements
   case queryExps of
-    (x :| xs) -> [|Query ($(return x) NE.:| $(return $ ListE xs))|]
+    (x :| xs) -> [|Query ($(pure x) NE.:| $(pure $ ListE xs))|]
 
 liftQuery :: SqlStatement -> Q Exp
 liftQuery stmt = do
   let allFragments = extractFragments stmt
-      (sqlString, varNames) = buildSqlAndVars allFragments 1
+      (sqlText, varNames) = buildSqlAndVars allFragments 1
   paramExps <- mapM generateParamExp varNames
   let paramList = ListE paramExps
-  [|SingleQuery (encodeUtf8 $(litE (stringL sqlString))) $(return paramList)|]
+  [|SingleQuery (encodeUtf8 $(litE (stringL (Text.unpack sqlText)))) $(pure paramList)|]
 
 -- | Walk through fragments in order, building the SQL string with $N placeholders
--- and collecting the interpolated variable names.
-buildSqlAndVars :: [SqlFragment] -> Int -> (String, [String])
+-- and collecting the interpolated Haskell expressions.
+buildSqlAndVars :: [SqlFragment] -> Int -> (Text, [Text])
 buildSqlAndVars [] _ = ("", [])
-buildSqlAndVars (LiteralText t : rest) n =
+buildSqlAndVars (NonInterpolatedSqlFragment t : rest) n =
   let (restSql, restVars) = buildSqlAndVars rest n
-   in (Text.unpack t ++ restSql, restVars)
-buildSqlAndVars (InterpolatedVar var : rest) n =
+   in (t <> restSql, restVars)
+buildSqlAndVars (InterpolatedHaskellExpr var : rest) n =
   let (restSql, restVars) = buildSqlAndVars rest (n + 1)
-   in ("$" ++ show n ++ restSql, var : restVars)
+   in ("$" <> Text.pack (show n) <> restSql, var : restVars)
 
 -- | Extract SqlFragment objects from SqlStatement
 extractFragments :: SqlStatement -> [SqlFragment]
@@ -121,29 +126,42 @@ extractFragments (SqlStatement blocks) = concatMap parseBlock blocks
 
 parseBlock :: BlockOrNotBlock -> [SqlFragment]
 parseBlock (NotBlock text) = parseInterpolations text
-parseBlock (Block text) = [LiteralText text]
+parseBlock (Block text) = [NonInterpolatedSqlFragment text]
 
--- | Parse text to find #{variableName} interpolation patterns
+-- | Parse text to find #{haskellExpr} interpolation patterns and return those
+-- separated from the rest.
 parseInterpolations :: Text -> [SqlFragment]
-parseInterpolations text = go text []
+parseInterpolations txt = case A.parseOnly (A.many' fragmentP <* A.endOfInput) txt of
+  Right fragments -> fragments
+  Left err -> error $ "Hpgsql error parsing sql quasiquoter: " ++ err
   where
-    go txt acc
-      | Text.null txt = reverse acc
-      | Text.isPrefixOf "#{" txt = case Text.breakOn "}" (Text.drop 2 txt) of
-          ("", "") -> reverse acc
-          (_, "") -> reverse (LiteralText txt : acc)
-          (varName, rest) ->
-            let remaining = Text.drop 1 rest
-                fragment = InterpolatedVar (Text.unpack varName)
-             in if Text.null varName
-                  then go remaining (LiteralText "#{" : acc)
-                  else go remaining (fragment : acc)
-      | otherwise =
-          case Text.breakOn "#{" txt of
-            (prefix, "") -> reverse (LiteralText prefix : acc)
-            (prefix, suffix) -> go suffix (LiteralText prefix : acc)
+    fragmentP :: Parser SqlFragment
+    fragmentP = interpolationP <|> literalP
+
+    interpolationP :: Parser SqlFragment
+    interpolationP = do
+      _ <- A.string "#{"
+      -- We hope no Haskell expressions contain '}'
+      varName <- A.takeWhile1 (/= '}') <|> fail "Found empty #{} or #{ without closing bracket"
+      _ <- A.char '}' <|> fail "Found #{ with an expression but no closing bracket"
+      pure $ InterpolatedHaskellExpr varName
+
+    literalP :: Parser SqlFragment
+    literalP = NonInterpolatedSqlFragment . Text.concat <$> some literalChunk
+      where
+        literalChunk = A.takeWhile1 (/= '#') <|> hashNotFollowedByBrace
+        hashNotFollowedByBrace = do
+          _ <- A.char '#'
+          mc <- A.peekChar
+          case mc of
+            Just '{' -> fail "#{ found, leaving parsing to interpolationP"
+            _ -> pure "#"
+
+proxyOf :: a -> Proxy a
+proxyOf _ = Proxy
 
 -- | Generate a parameter expression for a captured variable
-generateParamExp :: String -> Q Exp
-generateParamExp varName =
-  [|(Nothing, toPgField $(varE (mkName varName)))|]
+generateParamExp :: Text -> Q Exp
+generateParamExp haskellExpr =
+  let name = mkName (Text.unpack haskellExpr)
+   in [|(toTypeOid (proxyOf $(varE name)), toPgField $(varE name))|]
