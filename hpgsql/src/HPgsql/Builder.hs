f27c5451d7dcd671f71672b3020adb1601d9011d
Marcelo Zabani <mzabani@gmail.com>
2026-04-15 16:42:27 -0300

    Stop using `Sum` in Builder
    Makes no runtime difference, but is a bit cleaner

--- hpgsql/src/HPgsql/Builder.hs
+++ hpgsql/src/HPgsql/Builder.hs
@@ -14,7 +14,6 @@ import qualified Data.ByteString.Builder as Builder
 import qualified Data.ByteString.Builder.Extra as Builder
 import qualified Data.ByteString.Lazy as LBS
 import Data.Int (Int16, Int32, Int64)
-import Data.Monoid (Sum (..))
 import Data.Word (Word8)
 
 data BinaryField = SqlNull | NotNull !ByteString
@@ -24,50 +23,50 @@ instance Show BinaryField where
   show SqlNull = "NULL"
   show (NotNull bs) = show bs
 
-data LengthAwareBuilder = LengthAwareBuilder !(Sum Int32) !Builder.Builder
+data LengthAwareBuilder = LengthAwareBuilder !Int32 !Builder.Builder
 
 type Builder = LengthAwareBuilder
 
 instance Semigroup LengthAwareBuilder where
-  LengthAwareBuilder l1 b1 <> LengthAwareBuilder l2 b2 = LengthAwareBuilder (l1 <> l2) (b1 <> b2)
+  LengthAwareBuilder l1 b1 <> LengthAwareBuilder l2 b2 = LengthAwareBuilder (l1 + l2) (b1 <> b2)
 
 instance Monoid LengthAwareBuilder where
-  mempty = LengthAwareBuilder (Sum 0) mempty
+  mempty = LengthAwareBuilder 0 mempty
 
 builderLength :: LengthAwareBuilder -> Int32
-builderLength (LengthAwareBuilder (Sum n) _) = n
+builderLength (LengthAwareBuilder n _) = n
 
 binaryField :: BinaryField -> LengthAwareBuilder
 binaryField = \case
-  SqlNull -> LengthAwareBuilder (Sum 4) (Builder.int32BE (-1))
-  NotNull val -> let valLen = fromIntegral (BS.length val) in LengthAwareBuilder (Sum $ 4 + valLen) (Builder.int32BE valLen <> Builder.byteString val)
+  SqlNull -> LengthAwareBuilder 4 (Builder.int32BE (-1))
+  NotNull val -> let valLen = fromIntegral (BS.length val) in LengthAwareBuilder (4 + valLen) (Builder.int32BE valLen <> Builder.byteString val)
 
 toLazyByteString :: LengthAwareBuilder -> LBS.ByteString
-toLazyByteString (LengthAwareBuilder (Sum (fromIntegral -> totalLen)) builder) = Builder.toLazyByteStringWith (Builder.untrimmedStrategy totalLen 0) mempty builder
+toLazyByteString (LengthAwareBuilder (fromIntegral -> totalLen) builder) = Builder.toLazyByteStringWith (Builder.untrimmedStrategy totalLen 0) mempty builder
 
 toStrictByteString :: LengthAwareBuilder -> ByteString
 toStrictByteString = LBS.toStrict . toLazyByteString
 
 char7 :: Char -> LengthAwareBuilder
-char7 c = LengthAwareBuilder (Sum 1) (Builder.char7 c)
+char7 c = LengthAwareBuilder 1 (Builder.char7 c)
 
 string7 :: String -> LengthAwareBuilder
-string7 s = LengthAwareBuilder (Sum $ fromIntegral $ length s) (Builder.string7 s)
+string7 s = LengthAwareBuilder (fromIntegral $ length s) (Builder.string7 s)
 
 word8 :: Word8 -> LengthAwareBuilder
-word8 s = LengthAwareBuilder (Sum 1) (Builder.word8 s)
+word8 s = LengthAwareBuilder 1 (Builder.word8 s)
 
 int16BE :: Int16 -> LengthAwareBuilder
-int16BE n = LengthAwareBuilder (Sum 2) (Builder.int16BE n)
+int16BE n = LengthAwareBuilder 2 (Builder.int16BE n)
 
 int32BE :: Int32 -> LengthAwareBuilder
-int32BE n = LengthAwareBuilder (Sum 4) (Builder.int32BE n)
+int32BE n = LengthAwareBuilder 4 (Builder.int32BE n)
 
 int64BE :: Int64 -> LengthAwareBuilder
-int64BE n = LengthAwareBuilder (Sum 8) (Builder.int64BE n)
+int64BE n = LengthAwareBuilder 8 (Builder.int64BE n)
 
 byteString :: ByteString -> LengthAwareBuilder
-byteString bs = LengthAwareBuilder (Sum $ fromIntegral $ BS.length bs) (Builder.byteString bs)
+byteString bs = LengthAwareBuilder (fromIntegral $ BS.length bs) (Builder.byteString bs)
 
 lazyByteString :: LBS.ByteString -> LengthAwareBuilder
-lazyByteString bs = LengthAwareBuilder (Sum $ fromIntegral $ LBS.length bs) (Builder.lazyByteString bs)
+lazyByteString bs = LengthAwareBuilder (fromIntegral $ LBS.length bs) (Builder.lazyByteString bs)
