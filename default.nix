6b13040de90ea82426ad5a418246ec4c42481df5
Marcelo Zabani <mzabani@gmail.com>
2026-07-02 17:19:48 -0300

    Make PGHOST a top-level argument so we can run tests with different socket types

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
     system ? builtins.currentSystem
   , threading ? builtins.getEnv "HPGSQL_THREADED"
   , ghc ? "ghc9103"
+  , PGHOST ? builtins.getEnv "PGHOST"
   , pkgs ? import ./nix/nixpkgs.nix { inherit system threading; }
 }:
 let
@@ -18,22 +19,22 @@ rec {
   hpgsql-benchmarks = haskellPackages.hpgsql-benchmarks;
   inherit haskellPackages;
 
-  hpgsqlSimpleCompatTestsPg18 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_18; };
-  hpgsqlSimpleCompatTestsPg17 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_17; };
-  hpgsqlSimpleCompatTestsPg16 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_16; };
-  hpgsqlSimpleCompatTestsPg15 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_15; };
-  hpgsqlSimpleCompatTestsPg14 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_14; };
-  testsPg18 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_18; };
-  testsPg17 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_17; };
-  testsPg16 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_16; };
-  testsPg15 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_15; };
-  testsPg14 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs; postgres = addPgExtensions pkgs.postgresql_14; };
+  hpgsqlSimpleCompatTestsPg18 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_18; };
+  hpgsqlSimpleCompatTestsPg17 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_17; };
+  hpgsqlSimpleCompatTestsPg16 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_16; };
+  hpgsqlSimpleCompatTestsPg15 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_15; };
+  hpgsqlSimpleCompatTestsPg14 = { hspecArgs ? ""}: import ./nix/run-hpgsql-simple-compat-db-tests.nix { inherit pkgs hpgsql-simple-compat-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_14; };
+  testsPg18 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_18; };
+  testsPg17 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_17; };
+  testsPg16 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_16; };
+  testsPg15 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_15; };
+  testsPg14 = { hspecArgs ? ""}: import ./nix/run-db-tests.nix { inherit pkgs hpgsql-tests hspecArgs PGHOST; postgres = addPgExtensions pkgs.postgresql_14; };
 
   # Shells with specific-versioned postgres servers to run tests locally
-  shellPg18 = import ./nix/test-shell-pg.nix { inherit pkgs; postgres = addPgExtensions pkgs.postgresql_18; };
-  shellPg17 = import ./nix/test-shell-pg.nix { inherit pkgs; postgres = addPgExtensions pkgs.postgresql_17; };
-  shellPg16 = import ./nix/test-shell-pg.nix { inherit pkgs; postgres = addPgExtensions pkgs.postgresql_16; };
-  shellPg15 = import ./nix/test-shell-pg.nix { inherit pkgs; postgres = addPgExtensions pkgs.postgresql_15; };
-  shellPg14 = import ./nix/test-shell-pg.nix { inherit pkgs; postgres = addPgExtensions pkgs.postgresql_14; };
+  shellPg18 = import ./nix/test-shell-pg.nix { inherit pkgs PGHOST; postgres = addPgExtensions pkgs.postgresql_18; };
+  shellPg17 = import ./nix/test-shell-pg.nix { inherit pkgs PGHOST; postgres = addPgExtensions pkgs.postgresql_17; };
+  shellPg16 = import ./nix/test-shell-pg.nix { inherit pkgs PGHOST; postgres = addPgExtensions pkgs.postgresql_16; };
+  shellPg15 = import ./nix/test-shell-pg.nix { inherit pkgs PGHOST; postgres = addPgExtensions pkgs.postgresql_15; };
+  shellPg14 = import ./nix/test-shell-pg.nix { inherit pkgs PGHOST; postgres = addPgExtensions pkgs.postgresql_14; };
 }
   
