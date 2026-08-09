2d279c5d254bb93f510ad8fc1b4af55f6857debd
Marcelo Zabani <mzabani@gmail.com>
2026-08-08 23:09:15 -0300

    One INLINE pragma for great perf boost

--- hpgsql/src/Hpgsql/SimpleParser.hs
+++ hpgsql/src/Hpgsql/SimpleParser.hs
@@ -3,8 +3,11 @@
 -- and perform better than attoparsec, at least the way we use it in
 -- hpgsql.
 --
--- In benchmarks, this can improve performance by 12-15% materializing
--- query results.
+-- In benchmarks, this improved performance by 12-15% materializing
+-- query results when it was introduced.
+-- With the INLINE pragma in RowDecoder's Applicative's (<*>), GHC's
+-- inliner was finally able to make full use of continuation passing,
+-- and performance was improved by another ??? ?
 module Hpgsql.SimpleParser
   ( Parser (..),
     ParseResult (..),
