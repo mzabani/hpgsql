ef31fc04cfe05eee47f6ca724e81415a8786bcd5
Marcelo Zabani <mzabani@gmail.com>
2026-08-16 10:10:10 -0300

    Very experimental change with `Maybe a` instances

--- hpgsql/src/Hpgsql/SimpleParser.hs
+++ hpgsql/src/Hpgsql/SimpleParser.hs
@@ -32,6 +32,7 @@ module Hpgsql.SimpleParser
     takeFloatBE,
     takeDoubleBE,
     takeFloatBEWithFieldLength,
+    peekInt32BE,
   )
 where
 
@@ -41,7 +42,7 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Int (Int16, Int32, Int64)
 import Foreign.Storable (Storable)
-import GHC.Float (castWord32ToFloat, castWord64ToDouble, word2Float)
+import GHC.Float (castWord32ToFloat, castWord64ToDouble)
 import Hpgsql.Encoding.BinarySerializer (ByteStringIdx (..))
 import qualified Hpgsql.Encoding.BinarySerializer as BinSer
 import Prelude hiding (take)
@@ -154,6 +155,13 @@ takeInt32BE = Parser $ \idx bs kf ks ->
     Right v -> ks v (idx + 4) bs
     Left err -> kf err
 
+{-# INLINE peekInt32BE #-}
+peekInt32BE :: Parser Int32
+peekInt32BE = Parser $ \idx bs kf ks ->
+  case BinSer.decodeInt32BE idx bs of
+    Right v -> ks v idx bs
+    Left err -> kf err
+
 {-# INLINE takeInt32BEWithFieldLength #-}
 
 -- | Parses both a field length and the field itself, for
@@ -252,9 +260,9 @@ parseManyRows = Parser $ \idx' bs' _kf ks -> let (restIdx, nParsed) = go idx' bs
 {-# INLINE parseManyRows #-}
 
 -- | Succeeds only when the input has been fully consumed.
-endOfInput :: Parser ()
-endOfInput = Parser $ \idx bs kf ks ->
-  if BS.length bs <= idx.idx then ks () idx bs else kf "endOfInput: input remaining"
+endOfInput :: String -> Parser ()
+endOfInput debugFail = Parser $ \idx bs kf ks ->
+  if BS.length bs <= idx.idx then ks () idx bs else kf $ "endOfInput: input remaining (" ++ debugFail ++ ")"
 {-# INLINE endOfInput #-}
 
 -- | Run a parser and additionally return the slice of input it consumed.
